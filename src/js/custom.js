308031dee1c2e3de7b1d5dfdf4de6117e941e99e
Julian Killingback <jfkback@gmail.com>
2018-09-20 19:35:38 +0000

    Added slide out using jquery ui

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+    // $(".ck-button").click(function() {
+    //     $("#first-card").show("slide", { direction: "left" }, 1000);
+    //   //   $("#first-card").animate(
+		// 	// {"padding-right": "+=80%"},
+		// 	// "slow");
+    //
+    // });
     $(".ck-button").click(function() {
-        $(".row").animate(
-			{"padding-right": "+=20em", "opacity": "0"}, 
-			"slow");
+      $("#first-card").toggle("slide");
+      $("#first-card-2").removeAttr("hidden");
+      $("#first-card-2").toggle("slide");
     });
 });
