0802c8adec99e61fcadee4ad85338a693ed043a9
Julian Killingback <jfkback@gmail.com>
2018-09-24 20:07:34 +0000

    Added post pop-up and changed color of hover on nav bar

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,9 +6,19 @@ $(document).ready(function(){
 		// 	// "slow");
     //
     // });
+    var hideoptions = {  "direction" : "left",  "mode" : "hide"};
+    var showoptions = {"direction" : "right","mode" : "show"};
     $(".ck-button").click(function() {
-      $("#first-card").toggle("slide");
-      $("#first-card-2").removeAttr("hidden");
-      $("#first-card-2").toggle("slide");
+      // $("#first-card").toggle("slide");
+      // $("#first-card-2").removeAttr("hidden");
+      // $("#first-card-2").toggle("slide");
+      // $( "#firstCard" ).effect( "slide", hideoptions, 1000); //notice, no callback
+      // $( "#firstCard2" ).effect( "slide", showoptions, 1000);
     });
+    $("#navPostLink").click(function() {
+      $("#postModal").modal();
+    });
+    $('#postModal').on('shown.bs.modal', function() {
+      $('#myInput').focus()
+    })
 });
