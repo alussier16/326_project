aa10cc7517eed91bb1862c729d457e836b3bcd59
Julian Killingback <jfkback@gmail.com>
2018-09-27 16:15:04 -0400

    updated custom.js

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,20 +1,4 @@
 $(document).ready(function(){
-    // $(".ck-button").click(function() {
-    //     $("#first-card").show("slide", { direction: "left" }, 1000);
-    //   //   $("#first-card").animate(
-		// 	// {"padding-right": "+=80%"},
-		// 	// "slow");
-    //
-    // });
-    var hideoptions = {  "direction" : "left",  "mode" : "hide"};
-    var showoptions = {"direction" : "right","mode" : "show"};
-    $(".ck-button").click(function() {
-      // $("#first-card").toggle("slide");
-      // $("#first-card-2").removeAttr("hidden");
-      // $("#first-card-2").toggle("slide");
-      // $( "#firstCard" ).effect( "slide", hideoptions, 1000); //notice, no callback
-      // $( "#firstCard2" ).effect( "slide", showoptions, 1000);
-    });
     $("#navPostLink").click(function() {
       $("#postModal").modal();
     });
