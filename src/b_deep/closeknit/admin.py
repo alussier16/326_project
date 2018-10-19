c75879ff7699d0b2ac468ac665757f1d167f6205
sivanmyers <sivanmyers@gmail.com>
2018-10-19 02:39:52 +0000

    added time stampls and registered models with admin.py

--- src/b_deep/closeknit/admin.py
+++ src/b_deep/closeknit/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from closeknit.models import User, Post
+from closeknit.models import User, Post, Comment, Reaction
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     model = User
-    list_display = ("username", "first_name","last_name", "display_friends")
+    list_display = ("first_name","last_name","username","email","friend_code","display_friends")
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     model = Post
+    list_display = ("author", "time_stamp", "text_content", "img_content")
+
+@admin.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    model = Comment
+    list_display = ("author", "time_stamp", "content")
+
+@admin.register(Reaction)
+class PostAdmin(admin.ModelAdmin):
+    model = Reaction
+    list_display = ("user", "time_stamp", "status")
--- src/b_deep/closeknit/models.py
+++ src/b_deep/closeknit/models.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30, default= 'SOME_NAME')
-    last_name = models.CharField(max_length=30, default = 'SOME_NAME')
+    '''Model Representing User'''
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254, unique=True)
     username = models.CharField(max_length=20, unique=True)
     friend_code = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
@@ -24,25 +26,26 @@ class Post(models.Model):
     author = models.ForeignKey('User', on_delete=models.CASCADE)
     text_content = models.TextField(max_length= 500, help_text = 'Whats on your mind?',default= 'SOME_TEXT')
     img_content = models.ImageField(upload_to =None,height_field=None,width_field=None,max_length=100, default='SOME_IMG')
-    time_stamp = models.DateTimeField(blank =True, default='2018-10-17T23:02:30Z')
+    time_stamp = models.DateTimeField(blank=True, auto_now_add=True)
     def __str__(self):
-        return self.user.username
+        return f"{self.author}, {self.time_stamp}"
     
     
 
 class Comment(models.Model):
     content = models.TextField(max_length=250)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    time_stamp = models.DateTimeField(blank =True, default='2018-10-17T23:02:30Z')
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(blank=True, auto_now_add=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.author}, {self.time_stamp}"
 
 
 class Reaction(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, default = None)
     user = models.ForeignKey('User', on_delete=models.CASCADE, default = None)
+    time_stamp = models.DateTimeField(blank=True, auto_now_add=True)
 
     REACTIONS = (('0', 'None'),
     ('1', 'React 1'),
@@ -51,5 +54,8 @@ class Reaction(models.Model):
     ('4', 'React 4'),
     ('5', 'React 5'))
     status = models.CharField(max_length=1,choices= REACTIONS,blank=True,default= '0')
+
+    def __str__(self):
+        return f"{self.user}, {self.time_stamp}"
 
 
