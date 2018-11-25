8e4f00eb5fae754908949faee1ca543795815e82
Gizem Cicekli <vagrant@vagrant.vm>
2018-11-25 02:40:03 +0000

    Added to forms.py, views.py, and settings template to incorporate Settings Forms.

--- project3/src/b_deep/closeknit/forms.py
+++ project3/src/b_deep/closeknit/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _ 
 from closeknit.models import UserAccount
 
 class AddFriendForm(forms.Form):
@@ -17,3 +20,88 @@ class AddFriendForm(forms.Form):
                 return username and friend_code
 
 
+
+###_________________________________________________________________________________________________###
+#The forms for the Settings page: respectively for Updating Email, Username, Friend Code & Password
+
+class SettingsEmailForm(forms.Form):
+    email = forms.EmailField(required = True)
+
+    def clean(self):
+        data = self.cleaned_data.get('email')
+
+        print("clean emaile girdik")
+        print(data)
+
+        #checks if email is valid and then if email already exists
+        if data:
+
+            print("xd data dogru")
+
+            if UserAccount.objects.filter(email = data):
+                raise ValidationError(_("This email is already in use. Please enter a different email."))
+
+        """
+        else:
+            print("invalid email bok")
+            raise ValidationError(_("Invalid email."))
+        """
+
+        #returns the cleaned data
+        return data
+
+class SettingsUsernameForm(forms.Form):
+    username = forms.CharField(help_text ="Enter a Username to Add")
+
+    def clean(self):
+        data = self.cleaned_data.get('username')
+
+        print("clean username girdik")
+        print(data)
+
+        if data:
+            if UserAccount.objects.filter(username = data):
+                raise ValidationError(_("This username is already in use. Please enter a different username."))
+
+        #returns the cleaned data
+        return data
+
+class SettingsFriendCodeForm(forms.Form):
+    friend_code = forms.CharField(help_text="Enter a Friend Code to Add")
+
+    def clean(self):
+        data = self.cleaned_data.get('friend_code')
+
+        print("clean friend code a girdik")
+        print(data)
+
+        if data:
+            if UserAccount.objects.filter(friend_code = data):
+                raise ValidationError(_("This friend code belongs to another user. Please select a different friend code."))
+
+        #returns the cleaned data
+        return data
+
+class SettingsPasswordForm(forms.Form):
+    old_password = forms.CharField(required = True)
+    new_password = forms.CharField(label = "New Password", required = True)
+    confirm_password = forms.CharField(label = "Confirm New Password", required = True)
+
+    def clean(self):
+        old_data = self.cleaned_data.get('old_password')
+        new_data = self.cleaned_data.get('new_password')
+        confirm_data = self.cleaned_data.get('confirm_password')
+
+        print("old_data: " + old_data)
+        print("new_data: " + new_data)
+        print("confirm_data: " + confirm_data)
+        print("user password: " + UserAccount.objects.get(pk=1).password)
+
+        if old_data != UserAccount.objects.get(pk=1).password:
+           raise ValidationError(_("Invalid current password.")) 
+        else:
+            if new_data != confirm_data:
+                raise ValidationError(_("New passwords do not match."))
+        
+        #returns the cleaned data
+        return new_data
--- project3/src/b_deep/closeknit/views.py
+++ project3/src/b_deep/closeknit/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 from django.views import generic
 from closeknit.models import UserAccount, Post, Comment, Reaction
 from closeknit.forms import AddFriendForm
+from closeknit.forms import SettingsEmailForm
+from closeknit.forms import SettingsUsernameForm
+from closeknit.forms import SettingsFriendCodeForm
+from closeknit.forms import SettingsPasswordForm
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def test(request):
@@ -51,10 +59,85 @@ def accountcreated(request):
     )
 
 def settings(request):
-    user=UserAccount.objects.get(pk=1)
-    return render(
-        request, 'settings.html', {'page': 'settings', 'user': user}
-    )
+    user = UserAccount.objects.get(pk=1)
+    fno = 0  #this is used in settings template to decide which error to display
+
+    #if email is being updated
+    if request.method == 'POST' and 'btnEmail' in request.POST:
+        
+        form = SettingsEmailForm(request.POST)
+        fno = 1
+        if form.is_valid():
+            user.email = form.cleaned_data
+            user.save()
+            return HttpResponse("amk email saved")
+
+    #if username is being updated
+    elif request.method == 'POST' and 'btnUsername' in request.POST:
+
+        form = SettingsUsernameForm(request.POST)
+        fno = 2
+        if form.is_valid():
+            user.username = form.cleaned_data
+            user.save()
+            return HttpResponse("amk username saved")
+
+    #if friend code is being updated
+    elif request.method == 'POST' and 'btnFriendCode' in request.POST:
+
+        form = SettingsFriendCodeForm(request.POST)
+        fno = 3
+        if form.is_valid():
+            user.friend_code = form.cleaned_data
+            user.save()
+            return HttpResponse("amk friend code saved")
+
+    #if password is being updated
+    elif request.method == 'POST' and 'btnPassword' in request.POST:
+
+        form = SettingsPasswordForm(request.POST)
+        fno = 4
+        if form.is_valid():
+            print("password form valid")
+            user.password = form.cleaned_data
+            user.save()
+            return HttpResponse("amk password saved")
+   
+    else: 
+        fno = 0
+        form = SettingsEmailForm()
+        form = SettingsUsernameForm()
+        form = SettingsFriendCodeForm()
+        form = SettingsPasswordForm()
+  
+    
+    context = {'page': 'settings', 'user': user, 'form': form, 'fno': fno}
+
+    return render(request, 'settings.html', context)
+
+
+
+    """ TRIAL THAT WORKS ONLY FOR EMAIL i.e if one form on page
+    user = UserAccount.objects.get(pk=1)
+
+    if request.method == 'POST':
+
+        form = SettingsEmailForm(request.POST)
+
+        if form.is_valid():
+            user.email = form.cleaned_data
+            user.save()
+            return HttpResponse("amk email saved")
+
+    else: 
+        form = SettingsEmailForm()
+    
+    context = {'page': 'settings', 'user': user, 'form': form}
+
+    return render(request, 'settings.html', context)
+
+    #return render(request, 'settings.html', {'page': 'settings', 'user': user})
+    """
 
 def addfriend(request):
     user=UserAccount.objects.get(pk=1)
